92f8c664ca3d7ad6be5d1b213cb26197e9a9edc7
zzh-1992 <ezzh1992@icloud.com>
2021-10-31 18:00:58 +0800

    Initial commit

---
+++ src/main/java/com/grapefruit/jenkinsdemo/Index.java
@@ -0,0 +1,27 @@
+/*
+ *Copyright @2021 Grapefruit. All rights reserved.
+ */
+
+package com.grapefruit.jenkinsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * index
+ *
+ * @author zhihuangzhang
+ * @version 1.0
+ * @date 2021-10-31 5:58 下午
+ */
+@RestController
+public class Index {
+
+    @GetMapping("/")
+    public String index() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:sss"));
+    }
+}
