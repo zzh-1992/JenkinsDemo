b5cd63f5f94fb55cd9f0d67e887d8f3c08a10ba7
Grapefruit <65478141+zzh-1992@users.noreply.github.com>
2021-11-07 21:16:01 +0800

    Update build.sh
    添加docker命令的相关注释

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@ echo "=============================================="
 echo "=========Begin to create docker image========="
 echo "=============================================="
 
+# 创建镜像
 docker build -t jenkinsdemo/jenkinsdemo:2.0 .
 
+# 使用镜像创建容器
 docker run --name jenkinsdemo2.0 -d -p 8888:8080 jenkinsdemo/jenkinsdemo:2.0
 
 echo "=============================================="
