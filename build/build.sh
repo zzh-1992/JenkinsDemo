ec38e2371d6683accf63de51926d31a2694c4b67
zzh-1992 <ezzh1992@icloud.com>
2021-11-07 17:09:18 +0800

    使用安装版jenkins部署服务

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Hello World !"
 
-docker build -t jenkinsdemo/jenkinsdemo:2.0 .
+docker build -t /var/lib/jenkins/workspace/MyFirstJenkinsDemo/jenkinsdemo/jenkinsdemo:2.0 .
 
 docker run --name jenkinsdemo -d -p 8888:8080 jenkinsdemo/jenkinsdemo:1.0
 
