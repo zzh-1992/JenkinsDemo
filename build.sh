dec7b5d483d9afe0f1e5cb1cca411910c2f94f47
zzh-1992 <ezzh1992@icloud.com>
2021-11-07 17:58:39 +0800

    修改创建命令

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "=============================================="
 echo "=========Begin to create docker image========="
 echo "=============================================="
 
-docker build -t /var/lib/jenkins/workspace/MyFirstJenkinsDemo/jenkinsdemo/jenkinsdemo:2.0 .
+docker build -t jenkinsdemo/jenkinsdemo:2.0 .
 
 docker run --name jenkinsdemo2.0 -d -p 8888:8080 jenkinsdemo/jenkinsdemo:2.0
 
