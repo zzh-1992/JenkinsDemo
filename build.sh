6409ae124b355126c0802e347bf9dc9bd6706382
Grapefruit <65478141+zzh-1992@users.noreply.github.com>
2021-12-11 16:51:45 +0800

    Update build.sh
    修改容器映射端口

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "=============================================="
 docker build -t jenkinsdemo/jenkinsdemo:2.0 .
 
 # 使用镜像创建容器
-docker run --name jenkinsdemo2.0 -d -p 8888:8080 jenkinsdemo/jenkinsdemo:2.0
+docker run --name jenkinsdemo2.0 -d -p 8888:8888 jenkinsdemo/jenkinsdemo:2.0
 
 echo "=============================================="
 echo "============success to run server!============"
